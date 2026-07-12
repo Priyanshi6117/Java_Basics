089a8344e18d1203f30ee0de9e316307c7ec4725
Python <priyanshichandak2706@gmail.com>
2026-07-13 00:32:09 +0530

    Basics

---
+++ Number_guessing_game.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Number_guessing_game {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Random ran=new Random();
+        int attempts=0;
+        int guess;
+        int random=ran.nextInt(1, 11);
+        do{
+            System.out.print("Enter your guess between 1-10: ");
+            guess=sc.nextInt();
+            attempts++;
+        }while(random!=guess);
+        System.out.println("Congratulations, You guessed it right!!!");
+        System.out.printf("The number was %d.\n",random);
+        System.out.printf("You took %d attempts.",attempts);
+        sc.close();
+    }
+}
